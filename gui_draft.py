ab496a670227b9ed436f6f11b33f927c7a481643
PetrovTimur <s02200459@gse.cs.msu.ru>
2023-04-11 20:08:56 +0300

    Rework GUI
    - Use grid instead of pack
    - Fix resize handling
    - Add command to buttons

--- gui_draft.py
+++ gui_draft.py
@@ -1,50 +1,59 @@
-import tkinter as tk
-        
-window = tk.Tk()
-window.title("Battleship_field")
-WIDTH = 1200/3
-HEIGHT = 900/3
-UNIT = WIDTH // 16
-#window.geometry(f'{WIDTH}x{HEIGHT}+200+100')
-#window["bg"] = "#355C7D"
-#window["bg"] = "#406D96"
-window["bg"] = "#406D96"
-frame_1 = tk.Frame(master=window, bg='#406D96', width=12*UNIT, height = UNIT)
-frame_2 = tk.Frame(master=window,bg='#406D96', width=12*UNIT, height = 5*UNIT)
+from tkinter import *
+from tkinter import ttk
 
-frame_1.pack(side='top', fill='both', expand=True)
-frame_11 = tk.Frame(master=frame_1, bg='#406D96', width=5*UNIT, height = 1*UNIT)
-frame_12 = tk.Frame(master=frame_1, bg='#406D96', width=5*UNIT, height = 1*UNIT)
-frame_11.pack(side='left', fill='both', padx = 1*UNIT, pady = 1*UNIT)
-frame_12.pack(side='top', fill='both', padx = 1*UNIT, pady = 1*UNIT)
+FIELD_SIZE = 10
 
-frame_2.pack(side='top', fill='both', expand=True)
-frame_21 = tk.Frame(master=frame_2, bg='black', width=5*UNIT, height = 5*UNIT)
-frame_22 = tk.Frame(master=frame_2, bg='black', width=5*UNIT, height = 5*UNIT)
-frame_21.pack(side='left', fill='both', expand=True, padx = 1*UNIT, pady = 1*UNIT)
-frame_22.pack(side='left', fill='both', expand=True, padx = 1*UNIT, pady = 1*UNIT)
 
-label_1 = tk.Label(master = frame_11, text = 'Fleet of the glorious username1', fg='yellow', bg='#355C7D')
-label_1.pack(side='left', fill='both', expand=True, padx = 1*UNIT, pady = 1*UNIT)
+def change(button):
+    button.state(['disabled'])
 
-label_2 = tk.Label(master = frame_12, text = 'Fleet of the inglorious username2', fg='yellow', bg='#355C7D')
-label_2.pack(side='left', fill='both', expand=True, padx = 1*UNIT, pady = 1*UNIT)
 
-my_cells = [[tk.Button(master=frame_21, text='', fg='yellow', bg='#355C7D') for i in range(10)] for j in range(10)]
-enemy_cells = [[tk.Button(master=frame_22, text='', fg='yellow', bg='#355C7D') for i in range(10)] for j in range(10)]
-for i in range(10):
-    frame_21.grid_columnconfigure(index=i, weight=1, minsize=UNIT//4)
-    frame_21.grid_rowconfigure(index=i, weight=1, minsize=UNIT//4)
-    frame_22.grid_columnconfigure(index=i, weight=1, minsize=UNIT//4)
-    frame_22.grid_rowconfigure(index=i, weight=1, minsize=UNIT//4)
-    for j in range(10):
-        my_cells[i][j].grid(row=j, column=i, sticky='nesw')
-        enemy_cells[i][j].grid(row=j, column=i, sticky='nesw')
-#for i in range(4):
-    #my_cells[0][i].configure(text='X', fg = 'red')
-    #my_cells[2][i].configure(text='O', fg = '#00EC00')
-    #enemy_cells[0][i].configure(text='X', fg = '#00EC00')
-    #enemy_cells[2][i].configure(text='O', fg = 'red')
-#window.attributes('-fullscreen', True)
-#window.bind("<Escape>")
-window.mainloop()
+root = Tk()
+root.title("Battleship")
+root.geometry('1280x720')
+
+s = ttk.Style()
+s.theme_use('default')
+s.configure('Blue.TFrame', background='#406D96')
+s.configure('Blue.TButton', width=3, background='#355C7D')
+
+mainframe = ttk.Frame(root, style='Blue.TFrame')
+mainframe.grid(column=0, row=0, sticky='nsew')
+
+label1 = ttk.Label(mainframe, text='name 1', background='#355C7D', foreground='yellow')
+label1.grid(column=0, row=0, columnspan=3)
+label2 = ttk.Label(mainframe, text='name 2', background='#355C7D', foreground='yellow')
+label2.grid(column=3, row=0, columnspan=3)
+
+frame1 = ttk.Frame(mainframe, width=800, height=800)
+frame1.grid(column=1, row=2, sticky='nsew')
+frame2 = ttk.Frame(mainframe, width=800, height=800)
+frame2.grid(column=4, row=2, sticky='nsew')
+
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+mainframe.columnconfigure((0, 2, 3, 5), weight=1, minsize=80)
+mainframe.columnconfigure((1, 4), weight=0, minsize=480)
+mainframe.rowconfigure((0, 1, 3), weight=1, minsize=80)
+mainframe.rowconfigure(2, weight=0, minsize=480)
+
+
+buttons1 = [[ttk.Button(frame1, style='Blue.TButton') for i in range(FIELD_SIZE)] for j in range(FIELD_SIZE)]
+buttons2 = [[ttk.Button(frame2, style='Blue.TButton') for i in range(FIELD_SIZE)] for j in range(FIELD_SIZE)]
+
+for i in range(FIELD_SIZE):
+    for j in range(FIELD_SIZE):
+        buttons1[i][j].grid(column=i, row=j, sticky='nsew')
+        buttons1[i][j].configure(command=lambda b=buttons1[i][j]: change(b))
+        buttons2[i][j].grid(column=i, row=j, sticky='nsew')
+        buttons2[i][j].configure(command=lambda b=buttons2[i][j]: change(b))
+
+frame2.rowconfigure('all', weight=1)
+frame2.columnconfigure('all', weight=1)
+
+frame1.rowconfigure('all', weight=1)
+frame1.columnconfigure('all', weight=1)
+
+root.mainloop()
