a94516121ccd70289e24e8aef498cd345804086e
PetrovTimur <s02200459@gse.cs.msu.ru>
2023-04-13 19:35:14 +0300

    Add templates/functions for classes

--- game.py
+++ game.py
@@ -17,28 +17,18 @@ class Field:
         self.alive = True
     #     TODO add right amount of ships
 
+    def update(self, coord):
+        self.cells[coord[0]][coord[1]] = 1
+    #     TODO update ships, check status
 
-def game_logic(fields, cords):
-    'gets cords (consists of string(cell) and int(who made a a shot)) process shots'
-    global PLAYER, ENEMY
-    fields.shoot(cords)
-    send_to_enemy(cords)
-    if cords.get_player() == ENEMY:
-        if fields.any_yours_ship():
-            return ENEMY_TURN, fields
-        else:
-            return LOSE, fields
-    else:
-        if fields.any_enemys_ship():
-            return PLAYERS_TURN, fields
-        else:
-            return WIN, fields
-        return PLAYERS_TURN, fields
-    return WIN, fields
+    def auto_place(self, func):
+        self.cells = func()
 
-
-def wait_for_enemy_turn():
-    return
+    def check_placed(self):
+        for ship in self.ships:
+            if not ship.placed:
+                return False
+        return True
 
 
 class Game:
@@ -48,32 +38,11 @@ class Game:
         self.player_field = Field()
         self.enemy_field = Field()
 
-ENEMY = 0
-PLAYER = 1
-
-WIN = 0
-LOSE = 1
-PLAYERS_TURN = 2
-ENEMY_TURN = 3
-
-fields = Fields([[0]*8]*8, [[0]*8]*8)
-state = None
-
-txt = "Try game for the first time!"
-window = Tk()
-window.title('battleship-game')
-window.geometry("300x250")
-window.protocol('WM_DELETE_WINDOW', exit_button_pressed)
-while True:
-    destroy_all(window)
-    start_batton = ttk.Button(text="Start", command=start_button_pressed)
-    start_batton.pack()
-    exit_button = ttk.Button(text="Exit", command=exit_button_pressed)
-    exit_button.pack()
-    lbl = Label(text=txt)
-    lbl.pack()
-
-    window.mainloop()
-    game(window)
-    if state == WIN:
-        txt = 'Congratulations!!!'
+    def turn(self, cell):
+        self.enemy_field.update(cell)
+        if not self.enemy_field.alive:
+            return 'WIN'
+        #
+        self.player_field.update(cell)
+        if not self.player_field.alive:
+            return 'LOSS'
