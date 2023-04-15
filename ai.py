bac094f77b51232c2426d0440f40bee2333f4381
PetrovTimur <s02200459@gse.cs.msu.ru>
2023-04-15 18:42:37 +0300

    Improve AI shooting algorithm

--- ai.py
+++ ai.py
@@ -1,5 +1,9 @@
 from random import randint
 
 
-def shot() -> (int, int):
-    return randint(0, 9), randint(0, 9)
+def shot(cells) -> (int, int):
+    coords = randint(0, 9), randint(0, 9)
+    while cells[coords[0]][coords[1]] < 0:
+        coords = randint(0, 9), randint(0, 9)
+
+    return coords
