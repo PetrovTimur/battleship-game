395fa66b29375ddc456b3f2dab3e085153c0e197
Neil Nenakhov <nil7nenahov72013@gmail.com>
2023-06-01 19:56:49 +0300

    Unittests for ship class in game.py

--- battleship/logic/game.py
+++ battleship/logic/game.py
@@ -1,3 +1,5 @@
+import unittest
+
 FIELD_SIZE = 10
 TOTAL_SHIPS = 10
 
@@ -18,6 +20,60 @@ class Ship:
             self.status[coord] = False
 
         self.placed = True
+
+
+class ShipPlaceTestCase(unittest.TestCase):
+
+    def test_place_1(self):
+        self.ship = Ship(3)
+        self.ship.place([(1,1), (1,2), (1,3)])
+        self.assertEqual(self.ship.status[(1,1)], False)
+        self.assertEqual(self.ship.status[(1,2)], False)
+        self.assertEqual(self.ship.status[(1,3)], False)
+        self.assertEqual(self.ship.placed, True)
+
+    def test_place_2(self):
+        self.ship = Ship(3)
+        self.ship.place([(5,5), (6,5), (7,5)])
+        self.assertEqual(self.ship.status[(5,5)], False)
+        self.assertEqual(self.ship.status[(6,5)], False)
+        self.assertEqual(self.ship.status[(7,5)], False)
+        self.assertEqual(self.ship.placed, True)
+
+    def test_place_3(self):
+        self.ship = Ship(2)
+        self.ship.place([(5,5), (4,5)])
+        self.assertEqual(self.ship.status[(5,5)], False)
+        self.assertEqual(self.ship.status[(4,5)], False)
+        self.assertEqual(self.ship.placed, True)
+
+
+
+class ShipHitTestCase(unittest.TestCase):
+
+    def test_place_1(self):
+        self.ship = Ship(3)
+        self.ship.place([(1,1), (1,2), (1,3)])
+        self.ship.hit((1,2))
+        self.assertEqual(self.ship.status[(1,2)], True)
+        self.assertEqual(self.ship.afloat, True)
+
+    def test_place_2(self):
+        self.ship = Ship(3)
+        self.ship.place([(5,5), (6,5), (7,5)])
+        self.ship.hit((7,5))
+        self.assertEqual(self.ship.status[(7,5)], True)
+        self.assertEqual(self.ship.afloat, True)
+
+    def test_place_3(self):
+        self.ship = Ship(2)
+        self.ship.place([(5,5), (4,5)])
+        self.ship.hit((5,5))
+        self.ship.hit((4,5))
+        self.assertEqual(self.ship.status[(5,5)], True)
+        self.assertEqual(self.ship.status[(4,5)], True)
+        self.assertEqual(self.ship.afloat, False)
+
 
 
 class Field:
